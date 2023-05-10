b97d11a6eccdcb4be1696844fb4aeba5398462e0
Jeff Goeders <jeff.goeders@gmail.com>
2023-05-10 09:07:09 -0700

    add delay file
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

---
+++ src/delay_activity.cpp
@@ -0,0 +1,31 @@
+void delay_exploration();
+void delay_challenge1();
+void delay_challenge2();
+void delay_challenge3();
+
+void delay_activity() {
+  // This function should call ONE of your functions below.
+  // Uncomment or comment the lines so that just the function
+  // you want to call is uncommented.
+
+  delay_exploration();
+  //   delay_challenge1();
+  //   delay_challenge2();
+  //   delay_challenge3();
+}
+
+void delay_exploration() {
+  // Add your code here for exploration activities
+}
+
+void delay_challenge1() {
+  // Add your code here for challenge 1
+}
+
+void delay_challenge2() {
+  // Add your code here for challenge 2
+}
+
+void delay_challenge3() {
+  // Add your code here for challenge 3
+}
