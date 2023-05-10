39bd6521418ff7a31ca7d34ca8a06a8c21f300f4
Jeff Goeders <jeff.goeders@gmail.com>
2023-05-10 09:14:04 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@ void led_activity();
 void delay_activity();
 
 int main() {
+  // This function should call ONE of the functions below.
+  // Uncomment or comment the lines so that just the function
+  // you want to call is uncommented.
+  
   led_activity();
   // delay_activity();
 
