1d372b1d7f3c6a6b8b71c32d2e254e10b358a511
Philip Lundrigan <lundrigan@byu.edu>
2024-06-13 08:09:41 -0700

    Update settings and code formatting

--- src/_setup.cpp
+++ src/_setup.cpp
@@ -5,12 +5,12 @@
 int main();
 
 void setup() {
-  Serial.begin(9600);
-  leds_init();
-  timer_init();
-  delay(1000);
-  main();
-  while (1)
-    ;
+    Serial.begin(9600);
+    leds_init();
+    timer_init();
+    delay(1000);
+    main();
+    while (1)
+        ;
 }
 void loop() {}
--- src/badge_activity.cpp
+++ src/badge_activity.cpp
@@ -1,5 +1,5 @@
 #include "ybadge.h"
 
 void badge_activity() {
-  // Add your code here to customize your badge how you would like it.
+    // Add your code here to customize your badge how you would like it.
 }
